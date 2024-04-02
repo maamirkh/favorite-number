fa3ff11503f34c5ebee7b63484db0e6cde5160da
maamirkh <maamirkhan1983@gmail.com>
2024-04-03 01:37:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+#! usr/bin/env node
+let favoriteNumber = 8; //this save your favorite number
+console.log(`my favorite number is ${favoriteNumber}.`); //this shows the message with your favorite number
+export {};
---
+++ app.ts
@@ -0,0 +1,3 @@
+#! usr/bin/env node
+let favoriteNumber: number= 8;//this save your favorite number
+console.log(`my favorite number is ${favoriteNumber}.`);//this shows the message with your favorite number
